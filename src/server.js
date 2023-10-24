ed6c263550961fb718b9ef560e1dffb5504a7d6c
Anjana Induwara <36652213+Devanjana360@users.noreply.github.com>
2023-10-25 05:01:48 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(compression());
 app.use(cors({
-        origin: ['http://localhost:5173', 'http://localhost:4173', 'https://beliverrfront.vercel.app'],
+        origin: ['http://localhost:5173', 'http://localhost:4173', 'https://beliverrfront.vercel.app','https://tiny-babka-20fb8d.netlify.app'],
         credentials: true
 }));
 
